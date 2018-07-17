6419071c15988294b5fd9a0ec51d8c367107663b
StIch0 <loluich@gmail.com>
2018-07-17 13:27:49 +0800

    Initial Commit

---
+++ AvtoInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvtoInfo
+//
+//  Created by pavelburdukovskii on 17/07/2018.
+//  Copyright © 2018 pavelburdukovskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
